371c937928cd876ad25cb43ea2d8d61ac7c8efb4
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-12-19 14:27:42 -0300

    feat: add relatoty router

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,9 @@ export const routes: Routes = [
         path: '', component: LayoutComponent, children: [
             {
                 path: '', loadComponent: () => import('./feature/home/home.component').then(component => component.HomeComponent)
+            },
+            {
+              path: 'relatory', loadComponent: () => import('./feature/relatory/relatory.component').then(component => component.RelatoryComponent)
             }
         ]
     }
