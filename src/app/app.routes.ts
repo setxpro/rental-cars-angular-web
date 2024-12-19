378201c226b2b9a941d914e39def8b6730c62e9d
Patrick Anjos <patrickpqdt87289@gmail.com>
2024-12-19 14:33:48 -0300

    modificacao nas rotas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -8,6 +8,9 @@ export const routes: Routes = [
                 path: '', loadComponent: () => import('./feature/home/home.component').then(component => component.HomeComponent)
             },
             {
+                path: 'rent', loadComponent: () => import('./feature/rent/rent.component').then(component => component.RentComponent)
+            },
+            {
               path: 'relatory', loadComponent: () => import('./feature/relatory/relatory.component').then(component => component.RelatoryComponent)
             }
         ]
